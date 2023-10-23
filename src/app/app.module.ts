295b3aeb53abf676cc4ee19007061155b6b23663
Obiora Igboanusi <cwertlinks@gmail.com>
2023-10-23 13:12:31 +0100

    Update app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,42 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardTopBarComponent } from './components/dashboard-top-bar/dashboard-top-bar.component';
+import { DashboardLayoutComponent } from './components/dashboard-layout/dashboard-layout.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { MatIconModule } from '@angular/material/icon';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { LandingPageComponent } from './pages/landing-page/landing-page.component';
+import { SignUpComponent } from './pages/sign-up/sign-up.component';
+import { TasksComponent } from './pages/tasks/tasks.component';
+import { NewTaskComponent } from './pages/new-task/new-task.component';
+import { TaskDetailsComponent } from './pages/task-details/task-details.component';
+import { SearchBarComponent } from './components/search-bar/search-bar.component';
+import { OngoingTasksComponent } from './components/ongoing-tasks/ongoing-tasks.component';
+import { TaskMetricsComponent } from './components/task-metrics/task-metrics.component';
+import { TeamMembersComponent } from './components/team-members/team-members.component';
+import { TaskCardComponent } from './components/task-card/task-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardTopBarComponent,
+    DashboardLayoutComponent,
+    SidebarComponent,
+    DashboardComponent,
+    LandingPageComponent,
+    SignUpComponent,
+    TasksComponent,
+    NewTaskComponent,
+    TaskDetailsComponent,
+    SearchBarComponent,
+    OngoingTasksComponent,
+    TaskMetricsComponent,
+    TeamMembersComponent,
+    TaskCardComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, MatIconModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-sidebar',
@@ -6,6 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./sidebar.component.css'],
 })
 export class SidebarComponent {
+  @Output() closeSidebar = new EventEmitter();
+
   links = [
     {
       name: 'Dashboard',
@@ -28,4 +30,9 @@ export class SidebarComponent {
       linkUrl: '/settings',
     },
   ];
+
+  @HostListener('document:click')
+  onClickedOutside() {
+    this.closeSidebar.emit();
+  }
 }
