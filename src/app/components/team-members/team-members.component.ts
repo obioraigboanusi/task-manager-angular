f054cf511b835d5524c2620983f8a12411c25fd5
Obiora Igboanusi <cwertlinks@gmail.com>
2023-10-23 14:35:31 +0100

    Implement team members component

--- src/app/components/team-members/team-members.component.ts
+++ src/app/components/team-members/team-members.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-team-members',
   templateUrl: './team-members.component.html',
-  styleUrls: ['./team-members.component.css']
+  styleUrls: ['./team-members.component.css'],
 })
 export class TeamMembersComponent {
-
+  members= Array.from({length:6})
 }
