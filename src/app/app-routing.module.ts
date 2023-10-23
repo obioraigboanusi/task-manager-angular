281bfac03e17199192ceb1a13e1d8c53b8fa474a
Obiora Igboanusi <cwertlinks@gmail.com>
2023-10-23 13:04:10 +0100

    Implement page routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingPageComponent } from './pages/landing-page/landing-page.component';
+import { TasksComponent } from './pages/tasks/tasks.component';
+import { TaskDetailsComponent } from './pages/task-details/task-details.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { SignUpComponent } from './pages/sign-up/sign-up.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LandingPageComponent },
+  { path: 'register', component: SignUpComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'tasks', component: TasksComponent },
+  { path: 'new-tasks', component: TasksComponent },
+  { path: 'tasks/:taskId', component: TaskDetailsComponent },
+  { path: '**', component: LandingPageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
